6df2df57984f33122e3e0b423e5ed27ce08b897c
Qnbie <alszenful@gmail.com>
2020-06-08 13:35:25 +0100

    Setting up SpringBoot

---
+++ backend/src/main/java/PurplePapaya/PurplePapaya/PurplePapayaApplication.java
@@ -0,0 +1,13 @@
+package PurplePapaya.PurplePapaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurplePapayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurplePapayaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/PurplePapaya/PurplePapaya/PurplePapayaApplicationTests.java
@@ -0,0 +1,13 @@
+package PurplePapaya.PurplePapaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurplePapayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
